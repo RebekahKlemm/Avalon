9d75b2c6267791851952b9ffd75c8972df53779d
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-12-03 20:37:03 -0600

    add UPDATEUSERLOGINSTATUS to action constants

--- actions/constants.js
+++ actions/constants.js
@@ -1,3 +1,7 @@
+// currently used
+export const UPDATEUSERLOGINSTATUS = 'UPDATEUSERLOGINSTATUS';
+
+// for reference
 export const ADD_USER = 'ADD_USER';
 export const RECEIVE_USERS = 'RECEIVE_USERS';
 export const REFRESH_USERS = 'REFRESH_USERS';
