487fa87bd5648d7fcada51769ccd5c8cb675a582
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-12-03 20:44:22 -0600

    add Waiting component to display while facebook login is in progress

--- components/containers/Waiting.js
+++ components/containers/Waiting.js
@@ -1,3 +1,47 @@
-/**
- * Created by rebekahklemm on 10/28/17.
- */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class Waiting extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            message: ""
+        };
+
+
+    }
+
+
+    render(){
+
+        return(
+            <div>
+                Waiting
+            </div>
+        )
+
+    }
+
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        // allUsers: state.users.allUsers
+    };
+}
+
+
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        // setSession: function(user){
+        //     dispatch(setSession(user));
+        // }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Waiting);
+
+
