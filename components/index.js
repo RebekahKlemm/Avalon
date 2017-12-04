1a4ef78831344e380f2e1266deeb8b11cc0fd890
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-12-03 20:47:12 -0600

    add Waiting component to routes

--- components/index.js
+++ components/index.js
@@ -1,42 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-
 import {App} from './App';
 import LoginContainer from './containers/LoginContainer';
-// import SignupContainer from './containers/SignupContainer';
-import Welcome from './Welcome';
-
-
+import Waiting from './containers/Waiting';
 import {Provider} from 'react-redux';
 import store from '../store';
-
-import axios from 'axios';
-
-//import actions that are dispatched to store onEnter
-// import {receiveUsers} from '../actions/users';
-
-
-// const onAppEnter = function () {
-//     Promise.all([
-//         axios.get('/api/users'),
-//
-//     ])
-//         .then(responses => responses.map(r => r.data))
-//         .then(([users]) => {
-//             store.dispatch(receiveUsers(users));
-//         })
-//
-// };
-
-
 
 ReactDOM.render(
     <Provider store={store}>
         <Router history={browserHistory}>
-            <Route path='/' component={App} >
-                <IndexRoute component={Welcome}/>
+            <Route path='/' component={App}>
+                <IndexRoute component={LoginContainer}/>
                 <Route path='/login' component={LoginContainer}/>
+                <Route path='/waiting' component={Waiting}/>
             </Route>
         </Router>
     </Provider>,
