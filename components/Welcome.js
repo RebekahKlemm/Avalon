69be241cfefb81de60fef74b1b56fc0702ae4242
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-12-08 17:10:50 -0600

    add query params to login route

--- components/Welcome.js
+++ components/Welcome.js
@@ -4,10 +4,10 @@ import {Link} from 'react-router';
 export default function () {
     return (
         <div>
-            <Link to="/login">
+            <Link to="/login?role=organizer">
                 <span className="btn btn-primary btn-block newGameButton">Start A New Game</span>
             </Link>
-            <Link to="/login">
+            <Link to="/login?role=joiner">
                 <span className="btn btn-primary btn-block joinGameButton">Join A Game</span>
             </Link>
         </div>
