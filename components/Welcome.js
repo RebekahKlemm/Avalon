d90808c8d4b96a583cf981078fb15c86a8a8c961
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-12-08 16:58:49 -0600

    add links in Welcome

--- components/Welcome.js
+++ components/Welcome.js
@@ -4,12 +4,12 @@ import {Link} from 'react-router';
 export default function () {
     return (
         <div>
-            <span className="btn btn-primary btn-block newGameButton">
-                <Link to="/login">Start A New Game</Link>
-            </span>
-            <span className="btn btn-primary btn-block joinGameButton">
-                <Link to="/login">Join A Game</Link>
-            </span>
+            <Link to="/login">
+                <span className="btn btn-primary btn-block newGameButton">Start A New Game</span>
+            </Link>
+            <Link to="/login">
+                <span className="btn btn-primary btn-block joinGameButton">Join A Game</span>
+            </Link>
         </div>
     )
 }
