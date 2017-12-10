e6013b581e75aadd123d1d9590378cc30f42b513
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-12-10 14:21:08 -0600

    add Game and Player models to index

--- Database/Models/index.js
+++ Database/Models/index.js
@@ -3,12 +3,17 @@
 const Character = require('./characterModel');
 const Mission = require('./missionModel');
 const MissionBoard = require('./missionBoardModel');
+const Game = require('./gameModel');
+const Player = require('./playerModel');
 
 // Set up Relationships
-
+Game.hasMany(Player);
+Character.hasOne(Player);
 
 module.exports = {
     Character: Character,
     Mission: Mission,
-    MissionBoard: MissionBoard
+    MissionBoard: MissionBoard,
+    Game : Game,
+    Player : Player
 };
